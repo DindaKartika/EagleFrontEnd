42213d899430cbe166f3b69e8839eaeb9818b88c
trastanechora <trastanechora@gmail.com>
2019-03-31 12:34:56 +0700

    update feature/profile but without functional, just interface. The rest is coming soon

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -3,6 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import SignUp from "../pages/signup";
 import SignIn from "../pages/signin";
 import Perusahaan from "../pages/perusahaan";
+import App from '../pages/map';
+import profile from '../pages/profile';
 // const MainRoute = () => {
 class MainRoute extends Component {
     render() {
@@ -12,8 +14,9 @@ class MainRoute extends Component {
                 <Route exact path="/signup" component={SignUp}/>
                 <Route exact path="/signin" component={SignIn}/>
                 <Route exact path="/perusahaan" component={Perusahaan}/>
+                <Route exact path="/map" component={App}/>
+                <Route exact path="/profile" component={profile}/>
                 {/* <Route exact path = "/allproduct" component={AllProduct}/> */}
-    
             </Switch>
         );
     }
