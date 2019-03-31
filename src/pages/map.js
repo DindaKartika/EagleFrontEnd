31065cb4da473272846c9090feb5cecfcc40a1a8
DindaKartika <dkartika0887@gmail.com>
2019-03-31 15:38:26 +0700

    Fitur map_view

--- src/pages/map.js
+++ src/pages/map.js
@@ -4,88 +4,55 @@ import ReactMapboxGl from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import Header from '../components/header_signin'
-import DateTime from "react-datetime";
+import SidebarMap from '../components/sidebarMap'
+import FilterMap from '../components/filter'
 
 const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g"
 });
 
+
+
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			filter: false,
-			hari: ""
+			date : new Date(),
+			kota : "",
+			tanaman : "",
+			sidebar : false
 		};
 
 		this.viewFilter = this.viewFilter.bind(this);
+		this.viewSidebar = this.viewSidebar.bind(this);
 	}
-
-  // onDrawCreate = ({ features }) => {
-  //   console.log(features);
-  //   console.log(features['0'].geometry.coordinates)
-  // };
-
-  // onDrawUpdate = ({ features }) => {
-  //   console.log({ features });
-	// };
 	
 	viewFilter(){
 		this.setState({filter:true})
 	}
 
-	input7Day(event) {
-		this.setState({
-			hari: DateTime.moment().add(7, "day").calendar()
-		});
-		console.log(DateTime.moment().add(7, "day").calendar())
+	viewSidebar(){
+		this.setState({sidebar:true})
+		;
 	}
 
   render() {
+		console.log(this.state.sidebar)
     return (
       <div className="App">
 				<div className="header">
 					<Header/>
 				</div>
 				<div className="search">
-					<form action="/search">
-						<input type="text" onFocus={this.viewFilter} placeholder="Search.." name="search"/>
-						<button type="submit"><img src={'http://www.clker.com/cliparts/W/V/Z/X/h/t/search-icon-marine-md.png'}/></button>
-						<div className="filters">
-							<div class="dropdown">
-								<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Kota
-								</button>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<button class="dropdown-item">Action</button>
-									<button class="dropdown-item">Another action</button>
-									<button class="dropdown-item">Something else here</button>
-								</div>
-							</div>
-							<div class="dropdown">
-								<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Jenis Tanaman
-								</button>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<button class="dropdown-item">Action</button>
-									<button class="dropdown-item">Another action</button>
-									<button class="dropdown-item">Something else here</button>
-								</div>
-							</div>
-							<div class="dropdown">
-								<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Waktu Panen
-								</button>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<span class="dropdown-item" onClick={e => this.input7Day(e)}>1 minggu</span>
-									<label class="dropdown-item">1 bulan</label>
-									<label class="dropdown-item">3 bulan</label>
-								</div>
-							</div>
-						</div>
+					<form onSubmit={e => e.preventDefault()}>
+						<input type="search" onFocus={this.viewFilter} placeholder="Cari" name="search"/>
+						<button type="submit" onClick={this.viewSidebar}><img src={'http://www.clker.com/cliparts/W/V/Z/X/h/t/search-icon-marine-md.png'}/></button>
+						{this.state.filter && <FilterMap/>}
 					</form>
 				</div>
+				{this.state.sidebar && <SidebarMap/>}
 				<div>
 					<Map
 						style="mapbox://styles/mapbox/streets-v9"
