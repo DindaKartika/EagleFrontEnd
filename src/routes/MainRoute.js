1bbdf6e291cfd2907289b3e09bcf1189542f2d51
DindaKartika <dkartika0887@gmail.com>
2019-04-01 14:43:30 +0700

    feature/map_details

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import SignUp from "../pages/signup";
 import SignIn from "../pages/signin";
 import Perusahaan from "../pages/perusahaan";
-import App from '../pages/map';
 import profile from '../pages/profile';
 import Keamanan from "../pages/keamanan";
 import Karir from "../pages/karir";
@@ -23,17 +22,16 @@ class MainRoute extends Component {
                 <Route exact path="/signup" component={SignUp}/>
                 <Route exact path="/signin" component={SignIn}/>
                 <Route exact path="/perusahaan" component={Perusahaan}/>
-                <Route exact path="/map" component={App}/>
                 <Route exact path="/profile" component={profile}/>
                 <Route exact path="/keamanan" component={Keamanan}/>
                 <Route exact path="/karir" component={Karir}/>
                 <Route exact path="/iklan" component={Iklan}/>
                 <Route exact path="/pasangiklan" component={PasangIklan}/>
                 <Route exact path="/newsfeed" component={NewsFeed}/>
-                <Route exact path="/map" component={Map}/>
+                <Route exact path="/maps" component={Map}/>
                 <Route exact path="/input-field" component={InputField}/>
                 <Route exact path="/landingpage" component={LandingPage}/>
-                <Route exact path="/farm/:id_farm" component={Farm}/>
+                <Route exact path="/maps/:id_farm" component={Farm}/>
                 {/* <Route exact path = "/allproduct" component={AllProduct}/> */}
             </Switch>
         );
