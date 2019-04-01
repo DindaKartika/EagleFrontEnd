1818f1557b194dfa18f8ce0f9c988585827f957a
DindaKartika <dkartika0887@gmail.com>
2019-04-01 10:23:24 +0700

    Update

--- src/pages/farm.js
+++ src/pages/farm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import Header from '../components/header_signin'
@@ -27,6 +27,22 @@ const Map = ReactMapboxGl({
     "pk.eyJ1IjoiZGthcnRpa2EiLCJhIjoiY2p0d3NocmEzMDB0ejN5bWhkb2l2Zm92diJ9.OmM7fJbSBdSGfHQG4BH-qw"
 });
 
+const polygonPaint = {
+  'fill-color': '#6F788A',
+  'fill-opacity': 0.7
+};
+
+const AllShapesPolygonCoords = [
+  [
+    [-0.13235092163085938, 51.518250335096376],
+    [-0.1174163818359375, 51.52433860667918],
+    [-0.10591506958007812, 51.51974577545329],
+    [-0.10831832885742188, 51.51429786349477],
+    [-0.12531280517578122, 51.51429786349477],
+    [-0.13200759887695312, 51.517823057404094]
+  ]
+];
+
 class Farm extends Component {
 	constructor(props) {
 		super(props);
@@ -35,9 +51,9 @@ class Farm extends Component {
 			date : new Date(),
 			kota : "",
 			tanaman : "",
-            sidebar : false,
-            center : [],
-            koordinat : []
+			sidebar : false,
+			center : [],
+			koordinat : []
 		};
 
 		this.viewFilter = this.viewFilter.bind(this);
@@ -45,74 +61,24 @@ class Farm extends Component {
     }
     
     componentDidMount = () =>{
-        const self = this;
-        console.log(window.location.pathname.slice(6))
-		axios
-		.get('http://0.0.0.0:5000/farms/' + window.location.pathname.slice(6))
-		.then(function(response){
-            self.setState({Farms: response.data});
-            console.log('Farms', response.data);
-            const koordinat = JSON.parse(response.data.coordinates)
-            console.log('coord jadi', koordinat)
-            self.setState({koordinat: koordinat})
-            const centers = JSON.parse(response.data.center)
-            console.log(centers)
-            self.setState({center:centers})
-
-            self.map = new mapboxgl.Map({
-                container: self.mapContainer,
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [-68.13734351262877, 45.137451890638886],
-                zoom: 5
-              });
-
-           
-						this.map.on('load', () => {
-							this.map.addLayer({
-								id: 'farms',
-								type: 'fill',
-								source: {
-									type: 'geojson',
-									data: {
-										type: 'Feature',
-										geometry: {
-											type: 'Polygon',
-											coordinates: [
-												[
-														[-67.13734351262877, 45.137451890638886],
-														[-66.96466, 44.8097],
-														[-68.03252, 44.3252],
-														[-69.06, 43.98],
-														[-70.11617, 43.68405],
-														[-70.64573401557249, 43.090083319667144],
-														[-70.75102474636725, 43.08003225358635],
-														[-70.79761105007827, 43.21973948828747],
-														[-70.98176001655037, 43.36789581966826],
-														[-70.94416541205806, 43.46633942318431],
-														[-71.08482, 45.3052400000002],
-														[-70.6600225491012, 45.46022288673396],
-														[-70.30495378282376, 45.914794623389355],
-														[-70.00014034695016, 46.69317088478567],
-														[-69.23708614772835, 47.44777598732787],
-														[-68.90478084987546, 47.184794623394396],
-														[-68.23430497910454, 47.35462921812177],
-														[-67.79035274928509, 47.066248887716995],
-														[-67.79141211614706, 45.702585354182816],
-														[-67.13734351262877, 45.137451890638886]
-												]
-										]
-										}
-									}
-								}
-							}) // ID metches `mapbox/streets-v9`
-				
-							this.map.setPaintProperty('farms', 'fill-color', '#faafee');
-						});
-		})
-		.catch(function(error){
-			console.log('error', error);
-		})
-	}
+			const self = this;
+			console.log(window.location.pathname.slice(6))
+			axios
+			.get('http://0.0.0.0:5000/farms/' + window.location.pathname.slice(6))
+			.then(function(response){
+				self.setState({Farms: response.data});
+				console.log('Farms', response.data);
+				const koordinat = JSON.parse(response.data.coordinates)
+				console.log('coord jadi', koordinat)
+				self.setState({koordinat: koordinat})
+				const centers = JSON.parse(response.data.center)
+				console.log(centers)
+				self.setState({center:centers})
+			})
+			.catch(function(error){
+				console.log('error', error);
+			})
+		}
 	
 	viewFilter(){
 		this.setState({filter:true})
@@ -144,6 +110,9 @@ class Farm extends Component {
 
   render() {
 		console.log(this.state.sidebar)
+		const {center, koordinat} = this.state
+		console.log('center', center)
+		console.log('koordinat', koordinat)
     return (
       <div className="App">
 				<div className="header">
@@ -179,9 +148,12 @@ class Farm extends Component {
 							height: "100vh",
 							width: "100vw"
 						}}
-						center={[-68.13734351262877, 45.137451890638886]}
+						center={center}
 						zoom={[12]}
 					>
+						<Layer type="fill" paint={polygonPaint}>
+							<Feature coordinates={koordinat} />
+						</Layer>
 					</Map>
 				</div>
       </div>
